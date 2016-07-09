874a4ad5435502184c910cc68b6fd26bd5b33ddd
Hack Reactor Students <accounts@hackreactor.com>
2016-07-08 22:01:27 -0700

    fnight branch

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -3,7 +3,9 @@ angular.module('shortly.links', [])
 .controller('LinksController', function ($scope, Links) {
   // Your code here
   $scope.data = {};
+  console.log('linkscontroller');
   Links.getAll().then(function(data) {
     $scope.data.links = data;
+    console.log("000000000000000000000" + data);
   });
 });
